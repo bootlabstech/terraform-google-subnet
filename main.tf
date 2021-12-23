9e059d0b57cc617dfcc0c5d19c7e8367e380ec13
arun-kumar34 <arun.kumar@bootlabstech.com>
2021-12-23 18:39:52 +0530

    feat(subnet): add initial code for subnet
    dynamic block for a secondary_cidr_range is added, which is optional

--- main.tf
+++ main.tf
@@ -7,8 +7,8 @@ resource "google_compute_subnetwork" "vpc_subnet" {
   dynamic "secondary_ip_range" {
     for_each = var.secondary_ip_range
     content {
-      range_name = secondary_ip_range.range_name
-      ip_cidr_range = secondary_ip_range.ip_cidr_range
+      range_name = secondary_ip_range.value.range_name
+      ip_cidr_range = secondary_ip_range.value.ip_cidr_range
     }
   }
 }
