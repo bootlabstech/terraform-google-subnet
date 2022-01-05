6857c65ead7dea6c363089f15e42c9e11148c2c4
aravindakumarbootlabs <79306078+aravindakumarbootlabs@users.noreply.github.com>
2022-01-06 03:38:03 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,6 @@
 resource "google_compute_subnetwork" "vpc_subnet" {
   name          = var.name
+  description   = var.description
   ip_cidr_range = var.cidr_range
   region        = var.region
   network       = var.network_id
